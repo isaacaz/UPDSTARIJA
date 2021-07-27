66f913fcef4bfb98c78d893bf15e0950828e8cf0
isaac aguilera <oiazisaac@gmail.com>
2021-07-27 10:14:05 -0400

    whatsapp

--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-modal',
@@ -9,17 +11,47 @@ import { ModalController } from '@ionic/angular';
 export class ModalPage implements OnInit {
 
   countrycode:string="591";
-  whatsappnumber:string="75111830";
-  url:string="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
-
-  constructor(private modalCtrl:ModalController) { }
+  whatsappnumber:string="";
+  url:string="";
+  constructor(private modalCtrl:ModalController,private platform:Platform,private browser:InAppBrowser) { }
 
   ngOnInit() {
+    //this.whatsappnumber="69300480";
+    //this.url="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
   }
   dismissModal(){
     this.modalCtrl.dismiss();
   }
-  openUrl(){
-    
+  openUrl(key){
+    switch (key) {
+      case 'ST':
+        this.whatsappnumber="69300480";
+        this.url="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
+        break;
+      case 'CJ':
+        this.whatsappnumber="69325133";
+        this.url="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
+        break;
+      case 'RG':
+        this.whatsappnumber="69316771";
+        this.url="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
+        break;
+      case 'PV':
+        this.whatsappnumber="75128080";
+        this.url="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
+        break;
+      case 'MI':
+        this.whatsappnumber="75148828";
+        this.url="https://wa.me/"+this.countrycode+this.whatsappnumber+"?text=Deseo realizar una consulta";
+        break;
+      default:
+        break; 
+    }  
+    this.browser.create(this.url, '_system')
   }
+  chat() {
+    this.platform.ready().then(() => {
+     window.open('https://api.whatsapp.com/send?phone=75148828')
+    });}
+    
 }
